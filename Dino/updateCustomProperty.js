57fd67590422ba8aeac7c17f7516b4e1e2ce6070
Greg Roques <greg.roques@gmail.com>
2021-12-29 12:59:51 -0500

    update

--- Dino/updateCustomProperty.js
+++ Dino/updateCustomProperty.js
@@ -1,11 +1,18 @@
+// ===================== gets our element property
 export function getCustomProperty(elem, prop) {
-  return parseFloat(getComputedStyle(elem).getPropertyValue(prop)) || 0
+  //getComputedStyle() returns an object containing all of the CSS properties of an element
+  //getPropertyValue() returns a DOMString containing the value of a specified CSS property
+  //parseFloat converts value to a decimal number
+  return parseFloat(getComputedStyle(elem).getPropertyValue(prop)) || 0;
 }
 
+// ===================== sets our element property
 export function setCustomProperty(elem, prop, value) {
-  elem.style.setProperty(prop, value)
+  //setProperty() sets a new value for a property on a CSS style
+  elem.style.setProperty(prop, value);
 }
 
+// ===================== uses the above two function
 export function incrementCustomProperty(elem, prop, inc) {
-  setCustomProperty(elem, prop, getCustomProperty(elem, prop) + inc)
+  setCustomProperty(elem, prop, getCustomProperty(elem, prop) + inc);
 }
